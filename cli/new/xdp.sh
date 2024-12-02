a524e40210370d2e4a940cc188419bd3499b5261
Javier Moya <jmoya@hey.com>
2024-12-02 15:42:27 +0100

    Cleanup

--- cli/new/xdp.sh
+++ cli/new/xdp.sh
@@ -29,24 +29,8 @@ if [ "$commit_name_bpftool" = "" ] || [ "$commit_name_libbpf" = "" ] || [ "$new_
 fi
 
 #constants
-#ACAP_SERVERS_LIST="$CLI_PATH/constants/ACAP_SERVERS_LIST"
-#BUILD_SERVERS_LIST="$CLI_PATH/constants/BUILD_SERVERS_LIST"
-#DEVICES_LIST_NETWORKING="$CLI_PATH/devices_network"
-#FPGA_SERVERS_LIST="$CLI_PATH/constants/FPGA_SERVERS_LIST"
-#GPU_SERVERS_LIST="$CLI_PATH/constants/GPU_SERVERS_LIST"
 MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
-#NETWORKING_DEVICE_INDEX="1"
-#NETWORKING_PORT_INDEX="1"
 WORKFLOW="xdp"
-
-#get devices number
-#if [ -s "$DEVICES_LIST_NETWORKING" ]; then
-#  source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST_NETWORKING"
-#fi
-
-#get hostname
-#url="${HOSTNAME}"
-#hostname="${url%%.*}"
 
 #define directories
 DIR="$MY_PROJECTS_PATH/$WORKFLOW/$commit_name_bpftool/$new_name"
@@ -68,9 +52,6 @@ fi
 #clone repository
 $CLI_PATH/common/git_clone_xdp $DIR $commit_name_bpftool $commit_name_libbpf
 
-#change to project directory
-#cd $DIR
-
 #save commit_name_bpftool
 echo "$commit_name_bpftool" > $DIR/XDP_BPFTOOL_COMMIT
 echo "$commit_name_libbpf" > $DIR/XDP_LIBBPF_COMMIT
@@ -90,26 +71,6 @@ cp $HDEV_PATH/templates/$WORKFLOW/vars.mk $DIR/lib/vars.mk
 #compile files
 #chmod +x $DIR/config_add
 #chmod +x $DIR/config_delete
-
-#get interface name
-#interface_name=$($CLI_PATH/get/get_nic_config $NETWORKING_DEVICE_INDEX $NETWORKING_PORT_INDEX DEVICE)
-
-#read SERVERS_LISTS excluding the current hostname
-#IFS=$'\n' read -r -d '' -a remote_servers < <(cat "$ACAP_SERVERS_LIST" "$BUILD_SERVERS_LIST" "$FPGA_SERVERS_LIST" "$GPU_SERVERS_LIST" | grep -v "^$hostname$" | sort -u && printf '\0')
-
-#get target host
-#target_host=""
-#connected=""
-#for server in "${remote_servers[@]}"; do
-#    # Check connectivity to the current server
-#    if [[ "$(check_connectivity "$interface_name" "$server")" == "1" ]]; then
-#        target_host="$server"
-#        break
-#    fi
-#done
-
-#update remote_server in config_parameters
-#sed -i "/^remote_server/s/xxxx-xxxxx-xx/$target_host/" "$DIR/config_parameters"
 
 #push files
 if [ "$push_option" = "1" ]; then 
