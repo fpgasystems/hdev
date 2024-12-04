f379bc47c17fffdbd2773eea56669e5ac2d8619d
Javier Moya <jmoya@hey.com>
2024-12-04 12:57:53 +0100

    Prints available drivers

--- cli/help/build_xdp.sh
+++ cli/help/build_xdp.sh
@@ -10,6 +10,14 @@ is_network_developer=$4
 
 XDP_BPFTOOL_COMMIT=$($CLI_PATH/common/get_constant $CLI_PATH XDP_BPFTOOL_COMMIT)
 
+#get available drivers from templates
+current_directory=$(pwd)
+cd $CLI_PATH
+cd ../templates/xdp/drivers
+#drivers=$(ls -l | awk '/^d/ {print $NF}' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $//')
+drivers=$(ls -l | awk '/^d/ {print $NF}' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $//' | awk -F', ' '{if (NF > 3) {print $1", "$2", "$3", ..."} else {print $0}}')
+cd $current_directory
+
 if [ "$is_nic" = "1" ] && [ "$is_network_developer" = "1" ]; then
     echo ""
     echo "${bold}$CLI_NAME build xdp [flags] [--help]${normal}"
@@ -18,6 +26,7 @@ if [ "$is_nic" = "1" ] && [ "$is_network_developer" = "1" ]; then
     echo ""
     echo "FLAGS:"
     echo "   ${bold}-c, --commit${normal}    - GitHub commit ID for bpftool (default: ${bold}$XDP_BPFTOOL_COMMIT${normal})."
+    echo "   ${bold}-d, --driver${normal}    - An XDP driver identifier to be compiled (${bold}$drivers${normal})."
     echo "       ${bold}--project${normal}   - Specifies your xdp project name."
     echo ""
     echo "   ${bold}-h, --help${normal}      - Help to use this command."
