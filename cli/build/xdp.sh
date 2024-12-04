45384c7ed2ee2610477ea3e79ba584a173f5dbe5
Javier Moya <jmoya@hey.com>
2024-12-04 09:42:18 +0100

    Cleanup

--- cli/build/xdp.sh
+++ cli/build/xdp.sh
@@ -18,7 +18,7 @@ fi
 
 #inputs
 commit_name_bpftool=$2
-commit_name_libbpf=$3
+commit_name_libbpf=$3 #unsed at the moment
 project_name=$5
 
 #all inputs must be provided
@@ -27,27 +27,14 @@ if [ "$commit_name_bpftool" = "" ] || [ "$commit_name_libbpf" = "" ] || [ "$proj
 fi
 
 #constants
-#BITSTREAM_NAME=$($CLI_PATH/common/get_constant $CLI_PATH ONIC_SHELL_NAME)
-#BITSTREAMS_PATH="$CLI_PATH/bitstreams"
-#DRIVER_NAME=$($CLI_PATH/common/get_constant $CLI_PATH ONIC_DRIVER_NAME)
 MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
-#NUM_JOBS="8"
 WORKFLOW="xdp"
 
 #define directories
 DIR="$MY_PROJECTS_PATH/$WORKFLOW/$commit_name_bpftool/$project_name"
-#SHELL_BUILD_DIR="$DIR/open-nic-shell/script"
-#DRIVER_DIR="$DIR/open-nic-driver"
-
-#platform_name to FDEV_NAME
-#FDEV_NAME=$(echo "$platform_name" | cut -d'_' -f2)
-
-#define shell
-#library_shell="$BITSTREAMS_PATH/$WORKFLOW/$commit_name_bpftool/${BITSTREAM_NAME%.bit}.$FDEV_NAME.$vivado_version.bit"
-#project_shell="$DIR/${BITSTREAM_NAME%.bit}.$FDEV_NAME.$vivado_version.bit"
 
 #compile driver
-echo "${bold}eBPF compilation (commit ID: $commit_name_libbpf)${normal}"
+echo "${bold}eBPF compilation (commit ID: $commit_name_bpftool)${normal}"
 echo ""
 echo "cd $DIR && make"
 echo ""
