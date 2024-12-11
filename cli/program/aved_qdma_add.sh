529f2862902f04730ddb9ef4a5ebbfccbd743983
Javier Moya <jmoya@hey.com>
2024-12-11 16:26:11 +0100

    Working version

--- cli/program/aved_qdma_add.sh
+++ cli/program/aved_qdma_add.sh
@@ -6,20 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 #inputs
-device_index=$1
-
-#check on device index
-MAX_NUM_DEVICES=$(cat $CLI_PATH/devices_acap_fpga | wc -l)
-if [ "$device_index" -lt 1 ] || [ "$device_index" -gt "$MAX_NUM_DEVICES" ]; then
-    echo ""
-    echo "Please, choose a valid device index."
-    echo ""
-    exit 1
-fi
-
-#get BDF
-upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
-bdf="${upstream_port%?}1"
+bdf=$1
 
 #split
 bus=$(echo "$bdf" | cut -d':' -f1)
@@ -27,9 +14,22 @@ device=$(echo "$bdf" | cut -d'.' -f1 | cut -d':' -f2)
 function=$(echo "$bdf" | cut -d'.' -f2)
 
 #core commands
+echo "echo 100 > /sys/bus/pci/devices/0000\:$bus\:$device.$function/qdma/qmax"
 echo 100 > /sys/bus/pci/devices/0000\:$bus\:$device.$function/qdma/qmax
+sleep 1
+echo ""
+
+echo "dma-ctl qdma$bus$device$function q add idx 0 mode mm dir bi"
 dma-ctl qdma$bus$device$function q add idx 0 mode mm dir bi
 sleep 1
+echo ""
+
+echo "dma-ctl qdma$bus$device$function q start idx 0 idx_ringsz 15 dir bi"
 dma-ctl qdma$bus$device$function q start idx 0 idx_ringsz 15 dir bi
 sleep 1
+echo ""
+
+echo "chmod 666 /dev/qdma$bus$device$function-MM-0"
 chmod 666 /dev/qdma$bus$device$function-MM-0
+sleep 1
+echo ""
