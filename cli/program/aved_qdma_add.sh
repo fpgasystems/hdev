3d459406fe393d81fad66791daf1c90533b9da81
Javier Moya <jmoya@hey.com>
2024-12-11 15:23:40 +0100

    Working version

--- cli/program/aved_qdma_add.sh
+++ cli/program/aved_qdma_add.sh
@@ -22,15 +22,14 @@ upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
 bdf="${upstream_port%?}1"
 
 #split
-bus=${bdf:0:2}
-device=${bdf:3:2}
-function=${bdf:6:1}
+bus=$(echo "$bdf" | cut -d':' -f1)
+device=$(echo "$bdf" | cut -d'.' -f1 | cut -d':' -f2)
+function=$(echo "$bdf" | cut -d'.' -f2)
 
 #core commands
-#eval "sudo echo 100 > /sys/bus/pci/devices/0000\:$bus\:$device.$function/qdma/qmax"
-echo "echo 100 > /sys/bus/pci/devices/0000\:$bus\:$device.$function/qdma/qmax"
-echo "dma-ctl qdma$bus$device$function q add idx 0 mode mm dir bi"
+echo 100 > /sys/bus/pci/devices/0000\:$bus\:$device.$function/qdma/qmax
+dma-ctl qdma$bus$device$function q add idx 0 mode mm dir bi
 sleep 1
-echo "dma-ctl qdma$bus$device$function q start idx 0 idx_ringsz 15 dir bi"
+dma-ctl qdma$bus$device$function q start idx 0 idx_ringsz 15 dir bi
 sleep 1
-echo "chmod 666 /dev/qdma$bus$device$function-MM-0"
+chmod 666 /dev/qdma$bus$device$function-MM-0
