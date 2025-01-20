cee1f30c386b0d5222541e7dfb3e94035a3491a3
Javier Moya <jmoya@hey.com>
2025-01-20 11:56:14 +0100

    shows interface name

--- cli/get/ifconfig.sh
+++ cli/get/ifconfig.sh
@@ -51,16 +51,38 @@ if [ "$flags" = "" ]; then
     for device_index in $(seq 1 $MAX_DEVICES); do 
         ip=$($CLI_PATH/get/get_nic_device_param $device_index IP)
         if [ -n "$ip" ]; then
+            ip0=$(echo "$ip" | cut -d'/' -f1)
+            ip1=$(echo "$ip" | cut -d'/' -f2)
             mac=$($CLI_PATH/get/get_nic_device_param $device_index MAC)
             add_0=$(split_addresses $ip $mac 0)
             add_1=$(split_addresses $ip $mac 1)
+            iface_name_0=$(ifconfig | grep -B1 "$ip0" | awk '/^[a-zA-Z0-9]/ {print $1}' | sed 's/://') 
+            iface_name_1=$(ifconfig | grep -B1 "$ip1" | awk '/^[a-zA-Z0-9]/ {print $1}' | sed 's/://')
+            #first interface
+            if [ -n "$iface_name_0" ]; then
+                output=$(ip link show "$iface_name_0")
+                if echo "$output" | grep -q "xdp"; then
+                    xdp_0="(ethxdp)"
+                fi
+                #format
+                iface_name_0=": $iface_name_0 $xdp_0"
+            fi
+            #second interface
+            if [ -n "$iface_name_1" ]; then
+                output=$(ip link show "$iface_name_1")
+                if echo "$output" | grep -q "xdp"; then
+                    xdp_1="(ethxdp)"
+                fi
+                #format
+                iface_name_1=" : $iface_name_1 $xdp_1"
+            fi
             name="$device_index" 
             name_length=$(( ${#name} + 1 ))
-            echo "$name: $add_0"
-            printf "%-${name_length}s %s\n" "" "$add_1"
+            echo "$name: $add_0 $iface_name_0"
+            printf "%-${name_length}s %s\n" "" "$add_1" "$iface_name_1"
         fi
     done
-    echo ""
+    #echo ""
 else
     #device_dialog_check
     result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
