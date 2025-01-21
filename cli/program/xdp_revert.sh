1ed8b4fdc70c1af34ca67be628a96e886bec750f
Javier Moya <jmoya@hey.com>
2025-01-21 17:39:30 +0100

    Adding xdp_revert

---
+++ cli/program/xdp_revert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+device_index=$1
+
+id=$($CLI_PATH/get/get_fpga_device_param $device_index id)
+#print table
+if [ -n "$id" ]; then
+    workflow=$($CLI_PATH/get/workflow -d $device_index)
+    workflow=$(echo "$workflow" $device_index | cut -d' ' -f2 | sed '/^\s*$/d')
+    if [ $workflow = "onicxdp" ]; then
+        #get interface name
+        ip=$($CLI_PATH/get/get_fpga_device_param $device_index IP)
+        ip0=$(echo "$ip" | cut -d'/' -f1)
+        iface_name_0=$(ifconfig | grep -B1 "$ip0" | awk '/^[a-zA-Z0-9]/ {print $1}' | sed 's/://')
+        
+        #stop XDP program
+        sudo $CLI_PATH/program/xdp_detach $iface_name_0
+    fi
+fi
