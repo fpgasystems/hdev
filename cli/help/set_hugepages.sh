bbee28f40dd299a28141b6e45d3b12170610b622
Javier Moya <jmoya@hey.com>
2025-02-20 11:24:23 +0100

    Add help

---
+++ cli/help/set_hugepages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+CLI_NAME=$1
+
+echo ""
+echo "${bold}$CLI_NAME set hugepages [--help]${normal}"
+echo ""
+echo "Sets the number of 2MB or 1G hugepages."
+echo ""
+echo "FLAGS:"
+echo "   ${bold}-n, --number${normal}    - Number of hugepages."
+echo "   ${bold}-s, --size${normal}      - Use 2M for 2-megabyte pages or 1G for 1-gigabyte pages."
+echo ""
+echo "   ${bold}-h, --help${normal}      - Help to use this command."
+echo ""
+#exit 1
