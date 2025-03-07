bfb9ff1f1b21601122064a8b58c8038e05a6b8b7
Javier Moya <jmoya@hey.com>
2025-03-07 03:25:51 +0100

    Not used

--- playbooks/run_root_homebrew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ANSIBLE_CONFIG="ansible_root.cfg" /opt/homebrew/bin/ansible-playbook \
-    --ask-vault-pass \
-    --inventory hosts \
-    --extra-vars "ansible_ssh_user=root" \
-    "$@"
