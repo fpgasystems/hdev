ab92643d39dcf2d88cb745c9d3d115a8f747cc37
Javier Moya <jmoya@hey.com>
2025-07-02 13:48:17 +0200

    Defines commands we can kill

--- cli/common/pkill.sh
+++ cli/common/pkill.sh
@@ -3,7 +3,24 @@
 process_name="$1"
 
 if [[ -z "$process_name" ]]; then
-    echo "Usage: $0 <process-name>"
+    exit 1
+fi
+
+# Define allowed process names
+allowed_commands=(
+  "sockperf server"
+)
+
+# Check if the input matches one of the allowed commands
+is_allowed=false
+for allowed in "${allowed_commands[@]}"; do
+    if [[ "$process_name" == "$allowed" ]]; then
+        is_allowed=true
+        break
+    fi
+done
+
+if [[ "$is_allowed" != true ]]; then
     exit 1
 fi
 
