38bf3bb93459b699293e2f40d879e11c1522ea9e
Javier Moya <jmoya@hey.com>
2025-07-07 18:03:02 +0200

    Working version

--- cli/common/gh_pr_check.sh
+++ cli/common/gh_pr_check.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#inputs
-GITHUB_CLI_PATH=$1       # path to the GitHub CLI, e.g., "/usr/bin/gh"
-GITHUB_REPO=$2           # e.g., "fpgasystems/SLASH"
-GITHUB_PR=$3             # e.g., "42"
+GITHUB_CLI_PATH=$1    # directory, e.g. /usr/bin
+GITHUB_REPO=$2        # repo like fpgasystems/hdev
+GITHUB_PR=$3          # PR number like 55
 
-#check if the PR exists
-exists=$("$GITHUB_CLI_PATH" pr view "$GITHUB_PR" --repo "$GITHUB_REPO" --json number 2>/dev/null | jq -r 'if has("number") then "1" else "0" end')
+$GITHUB_CLI_PATH/gh pr view $GITHUB_PR --repo $GITHUB_REPO > /dev/null 2>&1
 
-echo "$exists"
+if [ $? -eq 0 ]; then
+    exists=1
+else
+    exists=0
+fi
+
+echo $exists
