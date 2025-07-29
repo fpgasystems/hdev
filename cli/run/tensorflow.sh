11cc5963ea6bb13d95a061b4b35f88e362bd8e7d
Javier Moya <jmoya@hey.com>
2025-07-29 12:57:17 +0200

    Works for float32

--- cli/run/tensorflow.sh
+++ cli/run/tensorflow.sh
@@ -71,9 +71,9 @@ echo ""
 #run application
 echo "${bold}Running your Tensorflow application:${normal}"
 echo ""
-echo "python3 ./src/main.py float32 $config_string" # --device $device_index 
+echo "python3 ./src/main.py $config_string" # --device $device_index 
 echo ""
-python3 ./src/main.py float32 $config_string
+python3 ./src/main.py $config_string
 echo ""
 
 #author: https://github.com/jmoya82
