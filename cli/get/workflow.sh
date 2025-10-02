af84626d60aeb846ceefe35a964a30975e423f20
Javier Moya <jmoya@hey.com>
2025-10-02 09:54:54 +0200

    Using DRIVER_NAME constants

--- cli/get/workflow.sh
+++ cli/get/workflow.sh
@@ -14,50 +14,14 @@ if [ "$is_acap" = "0" ] && [ "$is_asoc" = "0" ] && [ "$is_fpga" = "0" ]; then
     exit
 fi
 
-#is_opennic(){
-#    local device_index=$1
-#
-#    # Get device MAC address
-#    MACs=$($CLI_PATH/get/get_fpga_device_param "$device_index" MAC)
-#    MAC0="${MACs%%/*}"
-#
-#    # Convert MAC address to lowercase
-#    MAC0=$(echo "$MAC0" | tr '[:upper:]' '[:lower:]')
-#
-#    # Get device IP address
-#    IPs=$($CLI_PATH/get/get_fpga_device_param "$device_index" IP)
-#    IP0="${IPs%%/*}"
-#
-#    # Use ifconfig and awk to check for both IP and MAC in the same interface block
-#    if ifconfig | awk -v ip="$IP0" -v mac="$MAC0" '
-#        BEGIN { found_ip = 0; found_mac = 0; }
-#        /inet/ && $2 == ip { found_ip = 1 }
-#        /ether/ && $2 == mac { found_mac = 1 }
-#        found_ip && found_mac { exit 0 }  # Found both IP and MAC, success
-#        END { exit (found_ip && found_mac ? 0 : 1) }  # Return success if both are found
-#    '; then
-#        echo "1"
-#    else
-#        echo "0"
-#    fi
-#}
-
 #constants
+COYOTE_DRIVER_NAME=$($CLI_PATH/common/get_constant $CLI_PATH COYOTE_DRIVER_NAME)
 DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
+ONIC_DRIVER_NAME=$($CLI_PATH/common/get_constant $CLI_PATH ONIC_DRIVER_NAME)
 
 #get hostname
 url="${HOSTNAME}"
 hostname="${url%%.*}"
-
-#check on ACAP or FPGA servers (server must have at least one ACAP or one FPGA)
-#acap=$($CLI_PATH/common/is_acap $CLI_PATH $hostname)
-#fpga=$($CLI_PATH/common/is_fpga $CLI_PATH $hostname)
-#if [ "$acap" = "0" ] && [ "$fpga" = "0" ]; then
-#    echo ""
-#    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
-#    echo ""
-#    exit
-#fi
 
 #check on DEVICES_LIST
 source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
@@ -70,6 +34,10 @@ multiple_devices=$($CLI_PATH/common/get_multiple_devices $MAX_DEVICES)
 
 #inputs
 read -a flags <<< "$@"
+
+#get actual filename (i.e. onik.ko without the path)
+COYOTE_DRIVER_NAME="${COYOTE_DRIVER_NAME%.ko}"
+ONIC_DRIVER_NAME="${ONIC_DRIVER_NAME%.ko}"
 
 #check on flags
 device_found=""
@@ -85,7 +53,7 @@ if [ "$flags" = "" ]; then
             #check on integrations
             driver_name=$(lspci -s $bdf -nnk | awk -F': ' '/Kernel driver in use:/ {print $2}')
             #opennic=$(is_opennic "$device_index")
-            if [ "$driver_name" = "onic" ]; then
+            if [ "$driver_name" = "$ONIC_DRIVER_NAME" ]; then
                 workflow="onic"
                 #check on XDP (check on first interface)
                 ip=$($CLI_PATH/get/get_fpga_device_param $device_index IP)
@@ -98,7 +66,7 @@ if [ "$flags" = "" ]; then
                     fi
                 fi
                 echo "$device_index: $workflow"
-            elif [ "$driver_name" = "coyote_driver" ]; then
+            elif [ "$driver_name" = "$COYOTE_DRIVER_NAME" ]; then
                 workflow="coyote"
                 echo "$device_index: $workflow"
             else
@@ -149,7 +117,7 @@ else
         #check on integrations
         #opennic=$(is_opennic "$device_index")
         driver_name=$(lspci -s $bdf -nnk | awk -F': ' '/Kernel driver in use:/ {print $2}')
-        if [ "$driver_name" = "onic" ]; then
+        if [ "$driver_name" = "$ONIC_DRIVER_NAME" ]; then
             workflow="onic"
             #check on XDP (check on first interface)
             ip=$($CLI_PATH/get/get_fpga_device_param $device_index IP)
@@ -162,7 +130,7 @@ else
                 fi
             fi
             echo "$device_index: $workflow"
-        elif [ "$driver_name" = "coyote_driver" ]; then
+        elif [ "$driver_name" = "$COYOTE_DRIVER_NAME" ]; then
             workflow="coyote"
             echo "$device_index: $workflow"
         else
