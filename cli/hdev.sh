c16fbb0fec2f56d4fee942ab461e61065b3abd1f
Javier Moya <jmoya@hey.com>
2025-10-03 06:22:56 +0200

    Using get .topo

--- cli/hdev.sh
+++ cli/hdev.sh
@@ -442,21 +442,7 @@ case "$command" in
         command_run $command_arguments_flags"@"$valid_flags
         ;;
       topo)
-        valid_flags="-h --help"
-        command_run $command_arguments_flags"@"$valid_flags
-        #legend
-        legend="${legend}${bold}${COLOR_ON1}NICs${COLOR_OFF}${normal}"
-        if [ "$is_acap" = "1" ] || [ "$is_asoc" = "1" ] || [ "$is_fpga" = "1" ]; then
-          legend="${legend} ${bold}${COLOR_ON2}Adaptive Devices${COLOR_OFF}${normal}"
-        fi
-        if [ "$is_gpu" = "1" ]; then
-          legend="${legend} ${bold}${COLOR_ON5}GPUs${COLOR_OFF}${normal}"
-        fi
-        #print legend
-        if [[ -n "$legend" ]]; then
-          echo -e "$legend"
-          echo ""
-        fi
+        source "$CLI_PATH/$command/.$arguments"
         ;;
       uuid)
         source "$CLI_PATH/$command/.$arguments"
