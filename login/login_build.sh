a2d2918146c6a604a80ca2a465fb4b1b763a2a33
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2026-05-07 15:01:58 +0200

    refactor(login): cleanup and simplify login script for build servers

--- login/login_build.sh
+++ login/login_build.sh
@@ -5,138 +5,47 @@ CLI_PATH="/opt/$CLI_NAME/cli"
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-#constants
-EMAIL=$($CLI_PATH/common/get_constant $CLI_PATH EMAIL)
-MTU_DEFAULT=$($CLI_PATH/common/get_constant $CLI_PATH MTU_DEFAULT)
-XILINX_TOOLS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XILINX_TOOLS_PATH)
-XRT_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XRT_PATH)
+print_os_info() {
+  # load OS release environment variables for eg. PRETTY_NAME, NAME and VERSION
+  local PRETTY_NAME NAME VERSION
+  source /etc/os-release
 
-#get username
-username=$(getent passwd ${SUDO_UID})
-username=${username%%:*}
+  #print operating system information
+  echo "The server ${bold}$(hostname -s)${normal} is ready to work with ${bold}$PRETTY_NAME${normal}:"
+  echo ""
+  echo "    Full OS version : ${bold}$NAME $VERSION${normal}"
+  echo "    Linux kernel    : ${bold}$(uname -r)${normal}"
+  echo "    Uptime          : ${bold}$(uptime -p)${normal}"
+}
 
-#check on root
+print_FPGA_tools_info() {
+  echo "Xilinx Tools (XRT, Vivado, Vitis, Vitis_HLS) are not loaded into the environment by default"
+  echo "Load them into the environment using:"
+  echo ""
+  echo "    Vivado, Vitis, Vitis_HLS    : ${bold}module load vivado/<release>${normal}"
+  # TODO: move xrt also to environment modules
+  echo "    XRT                         : ${bold}source /opt/hdev/cli/enable/xrt${normal}"
+  echo ""
+  echo "The 'module' command allows for easy swapping of Bash Environment Variables. See all modules that are available using: ${bold}module avail${normal}"
+  echo "More modules will follow in the future. If you have a request please send them to hacc@ethz.ch with the subject starting with '[Feature Request]'."
+}
+
+username="${SUDO_USER:-$(id -un)}"
+
+#skip welcome message for root
 if [ "$username" = "root" ]; then
-    exit 1
+  exit 1
 fi
 
-#get hostname
-url="${HOSTNAME}"
-hostname="${url%%.*}"
-
-#derived
-VIVADO_PATH="$XILINX_TOOLS_PATH/Vivado"
-VITIS_PATH="$XILINX_TOOLS_PATH/Vitis"
-VITIS_HLS_PATH="$XILINX_TOOLS_PATH/Vitis_HLS"
-
-#print welcome message (1/2)
 echo ""
 echo "${bold}Welcome, $username!${normal}"
 echo ""
 
-#print operating system information
-distributor=$(lsb_release -i | awk -F':' '{print $2}' | xargs)
-release=$(lsb_release -r | awk -F':' '{print $2}' | xargs)
-description=$(lsb_release -d | awk -F'\t' '{print $2}' | sed 's/^[^0-9]*//')
-codename=$(lsb_release -c | awk -F':' '{print $2}' | xargs)
-linux_kernel=$(uname -r)
-uptime_info=$(uptime -p)
-echo "The server ${bold}$hostname${normal} is ready to work with ${bold}$distributor $release${normal}:"
+print_os_info
 echo ""
-echo "    Description : ${bold}$description${normal}"
-echo "    Codename    : ${bold}$codename${normal}"
-echo "    Linux kernel: ${bold}$linux_kernel${normal}"
-echo "    Uptime      : ${bold}$uptime_info${normal}"
+print_FPGA_tools_info
+
+echo ""
+echo "${bold}Have a nice $(date +%A)!${normal}"
 echo ""
 
-echo "Xilinx Tools (XRT, Vivado, Vitis, Vitis_HLS) are not loaded into the environment by default"
-echo "Load them into the environment using:"
-echo ""
-echo "    Vivado, Vitis, Vitis_HLS    : ${bold}module load vivado/<release>${normal}"
-echo "    XRT                         : ${bold}source /opt/hdev/cli/enable/xrt${normal}"
-echo ""
-echo "The 'module' command allows for easy swapping of Bash Environment Variables. See all modules that are available using: ${bold}module avail${normal}"
-echo "More modules will follow in the future. If you have a request please send them to hacc@ethz.ch with the subject starting with '[Feature Request]'."
-
-#get installed versions
-cd $VIVADO_PATH
-versions=( *"/" )
-
-#enable Xilinx tools version
-version="none"
-if [[ -n "$version/" ]] && [[ $version != "none" ]]; then
-    #remove the last character, i.e. "/"
-    version=${version::-1} 
-
-    #copy the desired XRT version to user’s local and preserve /opt/xilinx/xrt structure (Xilinx workaroud)
-	mkdir -p /local/home/$username/xrt_${version}$XRT_PATH
-	cp -r $XRT_PATH"_"${version}/* /local/home/$username/xrt_${version}$XRT_PATH
-
-    echo ""
-
-	#source xrt
-    source /local/home/$username/xrt_${version}$XRT_PATH/setup.sh
-
-    #source tools
-	source $VIVADO_PATH/${version}/settings64.sh
-	source $VITIS_PATH/${version}/settings64.sh
-	source $VITIS_HLS_PATH/${version}/settings64.sh
-
-	#get XRT branch
-	branch=$($XILINX_XRT/bin/xbutil --version | grep -i -w 'Branch' | tr -d '[:space:]')
-
-	#print message
-	echo ""
-	if [[ -d $VITIS_PATH/${branch:7:6} ]]; then
-		#Vitis is installed
-		echo "The server is ready to work with Xilinx ${bold}${branch:7:6}${normal} release branch:"
-		echo ""
-		echo "    Xilinx Board Utility (xbutil)          : ${bold}$XILINX_XRT/bin${normal}"
-		echo "    Xilinx Tools (Vivado, Vitis, Vitis_HLS): ${bold}/tools/Xilinx${normal}"
-	elif [[ -d $VIVADO_PATH/${branch:7:6} ]]; then
-		#Vitis is not installed
-		echo "The server is ready to work with Xilinx ${bold}${branch:7:6}${normal} release branch:"
-		echo ""
-		echo "    Xilinx Board Utility (xbutil)       : ${bold}$XILINX_XRT/bin${normal}"
-		echo "    Xilinx Tools (Vivado, Vitis_HLS)    : ${bold}/tools/Xilinx${normal}"
-	else
-		echo "The server needs special care to operate with XRT normally (Xilinx tools are not properly installed)."
-		echo ""
-		echo "${bold}An email has been sent to the person in charge;${normal} we will let you know when XRT is ready to use again."
-		echo "Subject: $hostname requires special attention ($username): Xilinx tools are not properly installed" | sendmail $EMAIL
-	fi
-fi
-
-#change to home directory
-cd
-
-#set MTU on Mellanox interface
-while read -r line || [[ -n "$line" ]]; do
-  # Extract all MAC addresses from the line
-  # Match typical MAC patterns: xx:xx:xx:xx:xx:xx
-  macs=$(echo "$line" | grep -oE '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}' | awk '{print tolower($0)}')
-
-  for mac in $macs; do
-    # Find interface for this MAC
-    iface=$(for i in /sys/class/net/*; do
-      if [[ "$(cat "$i/address")" == "$mac" ]]; then
-        basename "$i"
-        break
-      fi
-    done)
-
-    if [[ -n "$iface" ]]; then
-      echo "Setting MTU $MTU_DEFAULT on interface $iface (MAC $mac)"
-      sudo ip link set dev "$iface" mtu $MTU_DEFAULT up
-    else
-      echo "No interface found for MAC $mac"
-    fi
-  done
-done < "/opt/hdev/cli/devices_network"
-echo ""
-
-#print welcome message (2/2)
-weekday=$(date +%A)
-#echo ""
-echo "${bold}Have a nice $weekday!${normal}"
-echo ""
